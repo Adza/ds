f31dac554eb40db8f307a0a8eef757f3047e33ad
ohri31 <mirza.ohranovic@gmail.com>
2015-10-26 17:02:57 +0100

    k

---
+++ js/float.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+  $( ".skew1" ).animate({
+    left: "+=150",
+    top: "+=150"
+  }, 800, function() {
+  });
+
+  $( ".skew2" ).animate({
+    left: "-=150",
+    top: "-=150"
+  }, 800, function() {
+  });
+
+  $( "#skew3" ).animate({
+    left: "+=150",
+    top: "+=150"
+  }, 800, function() {
+  });
+
+  $( "#skew4" ).animate({
+    left: "-=150",
+    top: "+=150"
+  }, 800, function() {
+  });
+
+  $( "#skew5" ).animate({
+    left: "+=200",
+    top: "-=200"
+  }, 800, function() {
+  });
+
+  $( "#skew6" ).animate({
+    left: "+=200",
+    top: "-=150"
+  }, 800, function() {
+  });
+
+  $( "#skew7" ).animate({
+    left: "-=150",
+    top: "+=150"
+  }, 800, function() {
+  });
+
+});
